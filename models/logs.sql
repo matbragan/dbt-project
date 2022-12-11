6b7651c071d1bc657158bc2688b1ddd971468039
matheus.bs.oliveira@gmail.com <matheus.bs.oliveira@gmail.com>
2022-12-11 12:22:29 +0000

    developments 2

--- models/logs.sql
+++ models/logs.sql
@@ -3,4 +3,6 @@ select
 from 
     {{target.schema}}_meta.dbt_audit_log
 where 
-    event_schema <> ''
+    1=1
+    and event_schema not like '%_meta'
+    and event_model <> 'logs'
--- models/products_orders.sql
+++ models/products_orders.sql
@@ -1,12 +1,7 @@
 {{
     config(
         materialized = 'table',
-        pre_hook=["
-            begin;
-            lock table {{target.schema}}.products_orders;
-        "],
         post_hook=["
-            commit;
             grant usage on schema {{target.schema}} to group analytics;
             grant select on table {{target.schema}}.products_orders to group analytics;
         "]
