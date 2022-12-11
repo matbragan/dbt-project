3e91a919f891e857a7739e7fe7e52b1b9da1818c
matheus.bs.oliveira@gmail.com <matheus.bs.oliveira@gmail.com>
2022-12-11 20:03:11 +0000

    macros

--- models/logs.sql
+++ models/logs.sql
@@ -1,7 +1,7 @@
 select 
     *
-from 
+from
     {{target.schema}}_meta.{{try_dbt_log()}}
 where 
     1=1
-    and event_schema not like '%{{var('non_schema')}}'
+    and event_schema not like '%{{var('non_schema')}}' 
